a6fd95273f27e643b8dc0dea13c177c52fe168bf
mdelarosa-netizen <mdelarosa@pioix.edu.ar>
2026-09-17 08:39:39 -0300

    login
    a

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,56 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client"
+import { useEffect, useState } from 'react';
+import Input from "./components/Input"
+import Boton from './components/Boton';
 
 export default function Home() {
+  const [correo, setCorreo] = useState("");
+  const [usuario, setUsuario] = useState("");
+  const [contra, setContra] = useState("");
+
+
+
+
+  const updateCorreo = (event) => {
+    setCorreo(event.target.value)
+
+  };
+  
+  const updateUser = (event) => {
+    setUsuario(event.target.value)
+
+  };
+
+  const updateContra = (event) => {
+    setContra(event.target.value)
+
+  };
+
+
+
+  function logear(params) {
+
+    if (correo == "" || contra == ""){
+      
+    }
+
+    fetch('http://localhost:3001/estudiantes')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
   return (
     <div>
-      <p>ingrese su numnero de telefono</p>
-      <input type="int" placeholder="1112345678" />
-      <p>ingrese su contraseña</p>
-      <input type="text" placeholder="%€@!!pass" />
+      <Input funcion={updateCorreo} text="Ingrese su correo"></Input>
+      <Input funcion={updateContra} text="Ingrese su contraseña"></Input>
+
+      <Boton funcion={logear} text="Iniciar sesion"></Boton>
+
+
+
+      <Input funcion={updateUser} text="Ingrese su usuario"></Input>
+      
+
     </div>
   );
 }
