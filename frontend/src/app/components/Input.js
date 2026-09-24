9798faa9640480a60304fc2e80250cf92d649373
mdelarosa-netizen <mdelarosa@pioix.edu.ar>
2026-09-24 08:37:09 -0300

    global
    deshabilita la opción de chat global

--- frontend/src/app/components/Input.js
+++ frontend/src/app/components/Input.js
@@ -2,9 +2,11 @@
 
 export default function Input({funcion, text, tipo}) {
 
-
+    const update = (event) => {
+        funcion(event.target.value)
+    }
     return(
-        <input type={tipo} onChange={funcion} placeholder={text}></input>
+        <input type={tipo} onChange={update} placeholder={text}></input>
 
     );
 }
